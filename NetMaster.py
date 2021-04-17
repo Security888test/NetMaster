88262c6e058acf28d41adc79ebe0e6b417d42b3a
Adler <57773679+AdlerROG@users.noreply.github.com>
2021-04-17 21:00:11 +0200

    Updated NetMaster.py
    - Patched color bugs
    - Added a IP/Domain Name status checker before scanning
    - Upgraded Version to 1.2

--- NetMaster.py
+++ NetMaster.py
@@ -5,210 +5,189 @@ from colorama import Fore, Back, Style, init
 import os
 
 def Menu():
-	print (Fore.RED + Style.BRIGHT + "   _   _      _  ___  ___          _            ")
+	print (Fore.WHITE + Style.BRIGHT + "   _   _      _  ___  ___          _            ")
 	print ("  | \ | |    | | |  \/  |         | |           ")
 	print ("  |  \| | ___| |_| .  . | __ _ ___| |_ ___ _ __ ")
 	print ("  | . ` |/ _ \ __| |\/| |/ _` / __| __/ _ \ '__|")
 	print ("  | |\  |  __/ |_| |  | | (_| \__ \ ||  __/ |   ")
 	print ("  \_| \_/\___|\__\_|  |_/\__,_|___/\__\___|_|   ")
 	print ("                                                ")
-	print ("               V 1.0 - Adler - 2020             ")
+	print ("               V 1.3 - Adler - 2020             ")
 	print ()
 
 def Ask():
 	print()
-	query = input(Fore.YELLOW + Style.BRIGHT + " Enter an IP Adress or a Domain Name to scan \n >>")
+	query = input(Fore.WHITE + Style.BRIGHT + " Enter an IP Adress or a Domain Name to scan \n >>")
 
 	if query == "":
-		print(" Error : Please enter a target.")
+		print(Fore.RED + Style.BRIGHT + " Error : Please enter a target.")
 	else:
-		print("")
-		print(" OVERALL INFORMATIONS :")
-		print("")
-		urlip = "http://ip-api.com/csv/" + query + "?fields=query&lang=en"
-	fileip = urllib.request.urlopen(urlip)
+		urlstatus = "http://ip-api.com/csv/" + query + "?fields=status&lang=en"
+		filestatus = urllib.request.urlopen(urlstatus)
 
-	for line in fileip:
-		dcd_ip = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " IP Adress : " + dcd_ip, end="")
+		for line in filestatus:
+			dcd_status = line.decode("utf-8")
 
-	urlmob = "http://ip-api.com/csv/" + query + "?fields=mobile&lang=en"
-	filemob = urllib.request.urlopen(urlmob)
-
-	for line in filemob:
-		dcd_mob = line.decode("utf-8")
-
-		if "false" in dcd_mob:
-			print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Mobile : No")
-		elif "true" in dcd_mob:
-			print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Mobile : Yes")
+		if "fail" in dcd_status:
+			print(Fore.RED + Style.BRIGHT + "\n Error : Invalid or unreachable target.")
+			Ask()
 		else:
-			print(Fore.YELLOW + Style.BRIGHT + " [*]" + Fore.WHITE + " Mobile : Unknown")
+			print(Fore.BLUE + Style.BRIGHT + "\n [i] " + Fore.WHITE + "Starting scan...")
 
-		urlisp = "http://ip-api.com/csv/" + query + "?fields=isp&lang=en"
-	fileisp = urllib.request.urlopen(urlisp)
-
-	for line in fileisp:
-		dcd_isp = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Internet Service Provider [I.S.P.] : " + dcd_isp, end="")
+			print("")
+			print(" OVERALL INFORMATIONS :")
+			print("")
 		
-		urlorg = "http://ip-api.com/csv/" + query + "?fields=org&lang=en"
-	fileorg = urllib.request.urlopen(urlorg)
+			urlip = "http://ip-api.com/csv/" + query + "?fields=query&lang=en"
+			fileip = urllib.request.urlopen(urlip)
 
-	for line in fileorg:
-		dcd_org = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Organisation : " + dcd_org, end="")
-		
-		urlas = "http://ip-api.com/csv/" + query + "?fields=as&lang=en"
-	fileas = urllib.request.urlopen(urlas)
+			for line in fileip:
+				dcd_ip = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " IP Adress : " + dcd_ip, end="")
 
-	for line in fileas:
-		dcd_as = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Autonomous System : " + dcd_as, end="")
+			urlmob = "http://ip-api.com/csv/" + query + "?fields=mobile&lang=en"
+			filemob = urllib.request.urlopen(urlmob)
 
-	urlasn = "http://ip-api.com/csv/" + query + "?fields=asname&lang=en"
-	fileasn = urllib.request.urlopen(urlasn)
+			for line in filemob:
+				dcd_mob = line.decode("utf-8")
 
-	for line in fileasn:
-		dcd_asn = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Autonomous System Name : " + dcd_asn, end="")
+				if "false" in dcd_mob:
+					print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Mobile : No")
+				elif "true" in dcd_mob:
+					print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Mobile : Yes")
+				else:
+					print(Fore.YELLOW + Style.BRIGHT + " [*]" + Fore.WHITE + " Mobile : Unknown")
 
-	print("")
-	print(" GEOGRAPHICAL INFORMATIONS :")
-	print("")
+			urlisp = "http://ip-api.com/csv/" + query + "?fields=isp&lang=en"
+			fileisp = urllib.request.urlopen(urlisp)
 
-	urlc = "http://ip-api.com/csv/" + query + "?fields=continent&lang=en"
-	filec = urllib.request.urlopen(urlc)
+			for line in fileisp:
+				dcd_isp = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Internet Service Provider [I.S.P.] : " + dcd_isp, end="")
+				
+			urlorg = "http://ip-api.com/csv/" + query + "?fields=org&lang=en"
+			fileorg = urllib.request.urlopen(urlorg)
 
-	for line in filec:
-		dcd_c = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Continent : " + dcd_c, end="")
+			for line in fileorg:
+				dcd_org = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Organisation : " + dcd_org, end="")
+				
+			urlas = "http://ip-api.com/csv/" + query + "?fields=as&lang=en"
+			fileas = urllib.request.urlopen(urlas)
 
-	urlcc = "http://ip-api.com/csv/" + query + "?fields=continentCode&lang=en"
-	filecc = urllib.request.urlopen(urlcc)
+			for line in fileas:
+				dcd_as = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Autonomous System : " + dcd_as, end="")
 
-	for line in filecc:
-		dcd_cc = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Continental Code : " + dcd_cc, end="")
+			urlasn = "http://ip-api.com/csv/" + query + "?fields=asname&lang=en"
+			fileasn = urllib.request.urlopen(urlasn)
 
-	urlnation = "http://ip-api.com/csv/" + query + "?fields=country&lang=en"
-	filenation = urllib.request.urlopen(urlnation)
+			for line in fileasn:
+				dcd_asn = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Autonomous System Name : " + dcd_asn, end="")
 
-	for line in filenation:
-		dcd_nation = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Country : " + dcd_nation, end="")
+			print("")
+			print(" GEOGRAPHICAL INFORMATIONS :")
+			print("")
 
-	urlcnation = "http://ip-api.com/csv/" + query + "?fields=countryCode&lang=en"
-	filecnation = urllib.request.urlopen(urlcnation)
+			urlc = "http://ip-api.com/csv/" + query + "?fields=continent&lang=en"
+			filec = urllib.request.urlopen(urlc)
 
-	for line in filecnation:
-		dcd_cnation = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Country Code : " + dcd_cnation, end="")
+			for line in filec:
+				dcd_c = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Continent : " + dcd_c, end="")
 
-	urlregion = "http://ip-api.com/csv/" + query + "?fields=regionName&lang=en"
-	fileregion = urllib.request.urlopen(urlregion)
+			urlcc = "http://ip-api.com/csv/" + query + "?fields=continentCode&lang=en"
+			filecc = urllib.request.urlopen(urlcc)
 
-	for line in fileregion:
-		dcd_region = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Region : " + dcd_region, end="")
+			for line in filecc:
+				dcd_cc = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Continental Code : " + dcd_cc, end="")
 
-	urlrcegion = "http://ip-api.com/csv/" + query + "?fields=region&lang=en"
-	filecregion = urllib.request.urlopen(urlrcegion)
+			urlnation = "http://ip-api.com/csv/" + query + "?fields=country&lang=en"
+			filenation = urllib.request.urlopen(urlnation)
 
-	for line in filecregion:
-		dcd_cregion = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Region code : " + dcd_cregion, end="")
+			for line in filenation:
+				dcd_nation = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Country : " + dcd_nation, end="")
 
-	urlrzip = "http://ip-api.com/csv/" + query + "?fields=zip&lang=en"
-	filezip = urllib.request.urlopen(urlrzip)
+			urlcnation = "http://ip-api.com/csv/" + query + "?fields=countryCode&lang=en"
+			filecnation = urllib.request.urlopen(urlcnation)
 
-	for line in filezip:
-		dcd_zip = line.decode("utf-8")
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " ZIP Code : " + dcd_zip, end="")
+			for line in filecnation:
+				dcd_cnation = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Country Code : " + dcd_cnation, end="")
 
-	print("")
-	print(" DNS INFORMATIONS :")
-	print("")
+			urlregion = "http://ip-api.com/csv/" + query + "?fields=regionName&lang=en"
+			fileregion = urllib.request.urlopen(urlregion)
 
-	urlrdnsa = "https://api.hackertarget.com/dnslookup/?q=" + query
-	filednsa = urllib.request.urlopen(urlrdnsa)
+			for line in fileregion:
+				dcd_region = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Region : " + dcd_region, end="")
 
-	for line in filednsa:
-		dcd_dnsa = line.decode("utf-8")
+			urlrcegion = "http://ip-api.com/csv/" + query + "?fields=region&lang=en"
+			filecregion = urllib.request.urlopen(urlrcegion)
 
-	if "try" in dcd_dnsa:
-		print(Fore.YELLOW + Style.BRIGHT + " Only available with Domain Names.")
-	else:
-		print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " " + dcd_dnsa)
-	
-	print("")
+			for line in filecregion:
+				dcd_cregion = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " Region code : " + dcd_cregion, end="")
 
-	print(Fore.GREEN + Style.BRIGHT + " Scan done." + Fore.WHITE)
-	print("")
-	rep = input(" Would you like to scan another target ? [Y/n] >>")
+			urlrzip = "http://ip-api.com/csv/" + query + "?fields=zip&lang=en"
+			filezip = urllib.request.urlopen(urlrzip)
 
-	if rep == "Y":
-		Ask()
-	elif rep == "y":
-		Ask()
-	elif rep == "Yes":
-		Ask()
-	elif rep == "yes":
-		Ask()
-	elif rep == "":
-		Ask()
-	elif rep == "n":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "N":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "No":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "no":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
+			for line in filezip:
+				dcd_zip = line.decode("utf-8")
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " ZIP Code : " + dcd_zip, end="")
 
-try:
-	if os.name == "nt":
-		os.system("cls")
-	else:
-		os.system("clear")
+			print("")
+			print(" DNS INFORMATIONS :")
+			print("")
 
-	Menu()
-	print( " Welcome on the NetMaster - By Adler - [V 1.0] \n")
-	Ask()
+			urlrdnsa = "https://api.hackertarget.com/dnslookup/?q=" + query
+			filednsa = urllib.request.urlopen(urlrdnsa)
 
-	print("")
-	print(Fore.GREEN + Style.BRIGHT + " Scan done." + Fore.WHITE)
-	print("")
-	rep = input(" Would you like to scan another target ? [Y/n] >>")
+			for line in filednsa:
+				dcd_dnsa = line.decode("utf-8")
 
-	if rep == "Y":
-		Ask()
-	elif rep == "y":
-		Ask()
-	elif rep == "Yes":
-		Ask()
-	elif rep == "yes":
-		Ask()
-	elif rep == "":
-		Ask()
-	elif rep == "n":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "N":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "No":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
-	elif rep == "no":
-		print (Fore.RED + Style.BRIGHT + "\n Exiting...")
-		exit()
+			if "try" in dcd_dnsa:
+				print(Fore.YELLOW + Style.BRIGHT + " Only available with Domain Names.")
+			else:
+				print(Fore.GREEN + Style.BRIGHT + " [+]" + Fore.WHITE + " " + dcd_dnsa)
+			
+			print("")
+			print(Fore.BLUE + Style.BRIGHT + " [i] " + Fore.WHITE + "Scan done." + Fore.WHITE)
 
-	time.sleep(2.5)
-except:
-	print(Fore.RED + "\n \n An error occured while processing. NetMaster is exiting...")
-	exit()
+			print("")
+			rep = input(" Would you like to scan another target ? [Y/n] >>")
+
+			if rep == "Y":
+				Ask()
+			elif rep == "y":
+				Ask()
+			elif rep == "Yes":
+				Ask()
+			elif rep == "yes":
+				Ask()
+			elif rep == "":
+				Ask()
+			elif rep == "n":
+				print (Fore.RED + Style.BRIGHT + "\n Exiting...")
+				exit()
+			elif rep == "N":
+				print (Fore.RED + Style.BRIGHT + "\n Exiting...")
+				exit()
+			elif rep == "No":
+				print (Fore.RED + Style.BRIGHT + "\n Exiting...")
+				exit()
+			elif rep == "no":
+				print (Fore.RED + Style.BRIGHT + "\n Exiting...")
+				exit()
+
+if os.name == "nt":
+	os.system("cls")
+else:
+	os.system("clear")
+
+Menu()
+print( " Welcome on the NetMaster - By Adler - [V 1.0] \n")
+Ask()
